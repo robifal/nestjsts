9c916d2019f425f2fcc3ca83ec4322683800d0f7
Robert <127435909+robifal@users.noreply.github.com>
2023-08-29 16:21:02 -0300

    Add post,delete e put

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Put, Delete, ParseIntPipe, Res, HttpStatus, Param, Body } from '@nestjs/common';
 import { CatsService } from './cats.service';
 import { Cat } from './interface/cat.interface';
+import { Response } from 'express';
 
 @Controller('cats')
 export class CatsController {
@@ -9,7 +10,44 @@ export class CatsController {
 
     @Get()
     findAll(): Cat[] {
-        return this.catsService.findall();
+        return this.catsService.findAll();
     }
 
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+        const gatoEncontrado = this.catsService.findById(id); 
+        if (gatoEncontrado) {
+            res.status(HttpStatus.OK).json(gatoEncontrado);
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
+    }
+
+    @Delete(':id')
+    remove(@Param('id', ParseIntPipe) id: number, @Res() res: Response) {
+        const indexGatoEncontrado = this.catsService.findIndexById(id);
+        if (indexGatoEncontrado >= 0) {
+            this.catsService.deleteByIndex(indexGatoEncontrado);
+            res.status(HttpStatus.NO_CONTENT).send();
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
+    }
+
+    @Post()
+    create(@Body() cat: Cat, @Res() res: Response) {
+        this.catsService.create(cat);
+        res.status(HttpStatus.CREATED).json(cat);
+    }
+    
+    @Put(':id')
+    update(@Param('id') id: number, @Body() cat: Cat, @Res() res: Response) {
+        const indexGatoEncontrado = this.catsService.findIndexById(id);  // Pass 'id' instead of the comparison function
+        if (indexGatoEncontrado >= 0) {
+            this.catsService.updateById(indexGatoEncontrado, cat);
+            res.status(HttpStatus.NO_CONTENT).send();
+        } else {
+            res.status(HttpStatus.NOT_FOUND).send();
+        }
+    }
 }
--- src/cats/cats.service.ts
+++ src/cats/cats.service.ts
@@ -6,8 +6,27 @@ export class CatsService {
 
     private readonly cats: Cat[] = [];
 
-    findall(): Cat[]{
+    findAll(): Cat[] {
         return this.cats; 
     }
-}
 
+    create(cat: Cat): void {
+        this.cats.push(cat);
+    }
+
+    findById(id: number): Cat {
+        return this.cats.find(cat => cat.id === id);
+    }
+
+    findIndexById(id: number): number {
+        return this.cats.findIndex(cat => cat.id === id);
+    }
+
+    deleteByIndex(index: number): void {
+        this.cats.splice(index, 1);
+    }
+
+    updateById(index: number, updatedCat: Cat): void {
+        this.cats[index] = updatedCat;
+    }
+}
